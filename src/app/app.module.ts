a0b8a818882de6f0ca6f4efdbd241fa4ca9737ea
Caleb Evans <caevans@redhat.com>
2024-08-01 09:28:13 -0600

    Create the hypha-ui Angular app and add a new-product-form

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { NewProductFormComponent } from './new-product-form/new-product-form.component';
+import { routes } from './app.routes';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NewProductFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
