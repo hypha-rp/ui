8910b5d0eb91327c15506a1c4a4e8b7e4ac99f4d
Caleb Evans <caevans@redhat.com>
2024-10-09 01:30:09 -0600

    When you hover over the status icon, it shows a tooltip with the status

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,10 +7,28 @@ import { MatTableModule } from '@angular/material/table';
 import { TestResultsTabComponent } from './components/tabs/results/results-tab.component';
 import { LocalTimezonePipe } from './pipes/local-timezone.pipe';
 import { MultiLineTextDialog } from './components/dialogs/multi-line-text-dialog/multi-line-text-dialog.component';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatButtonModule } from '@angular/material/button';
+import { DetailedResultsDialog } from './components/dialogs/results-details/results-details-dialog.component';
+import { TestCaseDetailsDialog } from './components/dialogs/testcase-details/testcase-details-dialog.component';
 
 @NgModule({
-  declarations: [TestResultsTabComponent, LocalTimezonePipe, MultiLineTextDialog],
-  imports: [CommonModule, MatSnackBarModule, MatIconModule, MatDialogModule, MatTableModule],
+  declarations: [
+    TestResultsTabComponent,
+    LocalTimezonePipe,
+    MultiLineTextDialog,
+    DetailedResultsDialog,
+    TestCaseDetailsDialog,
+  ],
+  imports: [
+    CommonModule,
+    MatSnackBarModule,
+    MatIconModule,
+    MatDialogModule,
+    MatTableModule,
+    MatTooltipModule,
+    MatButtonModule,
+  ],
   exports: [TestResultsTabComponent, LocalTimezonePipe],
 })
 export class SharedModule {}
